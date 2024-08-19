3a87c9f36df2f1d356d172aa2dd16576be732108
higorabreuf@gmail.com <higorabreuf@gmail.com>
2024-08-19 05:27:48 -0300

    feat: add router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+  ];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ChatComponent } from '../../chat/chat.component';
+import { HeaderComponent } from '../../header/header.component';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ChatComponent, HeaderComponent, SidebarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
