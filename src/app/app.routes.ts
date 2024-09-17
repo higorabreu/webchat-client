75555f4d32095e118c39321d5c4520c6f2fb85e9
higorabreuf@gmail.com <higorabreuf@gmail.com>
2024-09-16 23:06:54 -0300

    feat: add auth service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/auth/register/register.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, { username, password });
+  }
+
+  register(username: string, name: string, email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, { username, name, email, password });
+  }
+}
