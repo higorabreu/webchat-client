fe55562b6d4ff6173b68c6fe73d0c3d9f9677939
higorabreuf@gmail.com <higorabreuf@gmail.com>
2024-09-17 00:55:25 -0300

    feat: configure login and register forms to make http request

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  message: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        this.message = "Login successful!";
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        if (err.status === 404) {
+          this.errorMessage = "User not found.";
+        } else if (err.status === 401) {
+          this.errorMessage = "Invalid password.";
+        } else {
+          this.errorMessage = "Failed to login.";
+        }
+      }
+    });
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  username: string = '';
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  message: string = '';
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  checkPasswords() : boolean {
+    return this.password === this.confirmPassword;
+  }
+
+  register() {
+    if (!this.checkPasswords()) {
+      this.errorMessage = 'Passwords do not match.';
+      return;
+    }
+
+    this.authService.register(this.username, this.name, this.email, this.password).subscribe({
+      next: () => {
+        this.message = "Registration successful!";
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        if (err.status === 409) {
+          this.errorMessage = "Email or Username already registered.";
+        } else {
+          this.errorMessage = "Failed to register user.";
+        }
+      }
+    });
+  }
 }
